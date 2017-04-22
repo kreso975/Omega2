6143cc637c9331ccd41ecddf9e6de921bbb94638
kreso975 <kresimir.kokanovic@gmail.com>
2017-04-23 00:13:08 +0200

    Init

---
+++ examples/LCDtest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kokanovic
+ * Date: 21/04/2017
+ * Time: 20:31
+ */
+
+require_once '../omegaLCD.php';
+require_once '../Omega2lib.php';
+
+$lcdAddress = 0x3f;
+$ReadTemperature = Array ( "C" => "Celsius", "F" => "Fahrenheit");
+
+$LCD = new OmegaLCD( $lcdAddress );
+$Temperature = new Omega2;
+
+$LCD->lcdClear();
+$LCD->backlightOn();
+//$BayMax->backlightOff();
+$temp = $Temperature->read1Wtemperature("C");
+
+echo $temp;
+$writeThis = Array ("Temperature:",  trim($temp)." C" );
+
+$LCD->lcdDisplayStringList($writeThis);
